11062248cc746a07e1caf0b55fc80af5c5986a1e
Kevin Shen <kshen3778@gmail.com>
2025-12-12 11:27:21 -0500

    changed env variables for supabase

--- frontend/lib/supabase/admin_client.ts
+++ frontend/lib/supabase/admin_client.ts
@@ -4,7 +4,7 @@ if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
   throw new Error('Missing environment variable NEXT_PUBLIC_SUPABASE_URL');
 }
 
-if (!process.env.SUPABASE_SERVICE_ROLE_KEY) {
+if (!process.env.SUPABASE_SECRET_KEY) {
   throw new Error('Missing environment variable SUPABASE_SERVICE_ROLE_KEY');
 }
 
@@ -17,7 +17,7 @@ if (!process.env.SUPABASE_SERVICE_ROLE_KEY) {
  */
 export const supabaseAdmin = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY,
+  process.env.SUPABASE_SECRET_KEY,
   {
     auth: {
       autoRefreshToken: false,
